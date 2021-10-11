d043850e1027bd26ecd79d1f47b6bad84488480f
Sanchay Mittal <sanchaymittal@gmail.com>
2021-10-11 21:50:51 +0530

    add: actions for protocol actions

--- src/state/protocol/actions.ts
+++ src/state/protocol/actions.ts
@@ -1,12 +1,7 @@
-import { ProtocolData } from './reducer'
 import { createAction } from '@reduxjs/toolkit'
 import { ChartDayData, Transaction } from 'types'
-import { SupportedNetwork } from 'constants/networks'
 
 // protocol wide info
-export const updateProtocolData =
-  createAction<{ protocolData: ProtocolData; networkId: SupportedNetwork }>('protocol/updateProtocolData')
-export const updateChartData =
-  createAction<{ chartData: ChartDayData[]; networkId: SupportedNetwork }>('protocol/updateChartData')
-export const updateTransactions =
-  createAction<{ transactions: Transaction[]; networkId: SupportedNetwork }>('protocol/updateTransactions')
+export const updateProtocolData = createAction<{ totalVolume: number }>('protocol/updateProtocolData')
+export const updateChartData = createAction<{ chartData: ChartDayData[] }>('protocol/updateChartData')
+export const updateTransactions = createAction<{ transactions: Transaction[] }>('protocol/updateTransactions')
