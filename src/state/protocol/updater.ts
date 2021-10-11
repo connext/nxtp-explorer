d88a301bde60103c4dcb6a3085d08d6dd5b6b384
Sanchay Mittal <sanchaymittal@gmail.com>
2021-10-11 21:51:24 +0530

    add: hooks and updater

--- src/state/protocol/updater.ts
+++ src/state/protocol/updater.ts
@@ -1,47 +1,50 @@
-import { useProtocolData, useProtocolChartData, useProtocolTransactions } from './hooks'
+import { useProtocolData, useProtocolChartData } from './hooks'
 import { useEffect } from 'react'
-import { useFetchProtocolData } from 'data/protocol/overview'
-import { useFetchGlobalChartData } from 'data/protocol/chart'
-import { fetchTopTransactions } from 'data/protocol/transactions'
-import { useClients } from 'state/application/hooks'
+import { useFetchGlobalChartData } from 'subgraph'
+// import { fetchTopTransactions } from 'data/protocol/transactions'
 
 export default function Updater(): null {
   // client for data fetching
-  const { dataClient } = useClients()
-
-  const [protocolData, updateProtocolData] = useProtocolData()
-  const { data: fetchedProtocolData, error, loading } = useFetchProtocolData()
+  // const { dataClient } = useClients()
 
   const [chartData, updateChartData] = useProtocolChartData()
-  const { data: fetchedChartData, error: chartError } = useFetchGlobalChartData()
 
-  const [transactions, updateTransactions] = useProtocolTransactions()
+  const [totalVolume, updateTotalVolume] = useProtocolData()
+  const { data: fetchedChartData, error: chartError, totalVolume: fetchedTotalVolume } = useFetchGlobalChartData()
 
-  // update overview data if available and not set
-  useEffect(() => {
-    if (protocolData === undefined && fetchedProtocolData && !loading && !error) {
-      updateProtocolData(fetchedProtocolData)
-    }
-  }, [error, fetchedProtocolData, loading, protocolData, updateProtocolData])
+  // const [transactions, updateTransactions] = useProtocolTransactions()
+
+  // // update overview data if available and not set
+  // useEffect(() => {
+  //   if (protocolData === undefined && fetchedProtocolData && !loading && !error) {
+  //     updateProtocolData(fetchedProtocolData)
+  //   }
+  // }, [error, fetchedProtocolData, loading, protocolData, updateProtocolData])
 
   // update global chart data if available and not set
+  useEffect(() => {
+    if (totalVolume === undefined && fetchedTotalVolume && !chartError) {
+      updateTotalVolume(fetchedTotalVolume)
+    }
+  }, [totalVolume, chartError, fetchedTotalVolume, updateTotalVolume])
+
   useEffect(() => {
     if (chartData === undefined && fetchedChartData && !chartError) {
       updateChartData(fetchedChartData)
     }
   }, [chartData, chartError, fetchedChartData, updateChartData])
 
-  useEffect(() => {
-    async function fetch() {
-      const data = await fetchTopTransactions(dataClient)
-      if (data) {
-        updateTransactions(data)
-      }
-    }
-    if (!transactions) {
-      fetch()
-    }
-  }, [transactions, updateTransactions, dataClient])
+  // useEffect(() => {
+  //   async function fetch() {
+  //     const data = await fetchTopTransactions(dataClient)
+  //     if (data) {
+  //       updateTransactions(data)
+  //     }
+  //   }
+  //   if (!transactions) {
+  //     fetch()
+  //   }
+  // }, [transactions, updateTransactions, dataClient])
 
   return null
 }
