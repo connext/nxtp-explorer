f12886f273ee077a23d36f9ac428a35386ec0f12
Sanchay Mittal <sanchaymittal@gmail.com>
2021-10-11 21:49:47 +0530

    add: other apollo clients

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -63,3 +63,98 @@ export const polygonClient = new ApolloClient({
     },
   },
 })
+
+export const bscClient = new ApolloClient({
+  uri: getDeployedSubgraphUri(56),
+  cache: new InMemoryCache(),
+  queryDeduplication: true,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-first',
+    },
+    query: {
+      fetchPolicy: 'cache-first',
+      errorPolicy: 'all',
+    },
+  },
+})
+
+export const xdaiClient = new ApolloClient({
+  uri: getDeployedSubgraphUri(100),
+  cache: new InMemoryCache(),
+  queryDeduplication: true,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-first',
+    },
+    query: {
+      fetchPolicy: 'cache-first',
+      errorPolicy: 'all',
+    },
+  },
+})
+
+export const fantomClient = new ApolloClient({
+  uri: getDeployedSubgraphUri(250),
+  cache: new InMemoryCache(),
+  queryDeduplication: true,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-first',
+    },
+    query: {
+      fetchPolicy: 'cache-first',
+      errorPolicy: 'all',
+    },
+  },
+})
+
+export const arbitrumClient = new ApolloClient({
+  uri: getDeployedSubgraphUri(42161),
+  cache: new InMemoryCache(),
+  queryDeduplication: true,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-first',
+    },
+    query: {
+      fetchPolicy: 'cache-first',
+      errorPolicy: 'all',
+    },
+  },
+})
+
+export const avalancheClient = new ApolloClient({
+  uri: 'https://connext-avalanche-subgraph.apps.bwarelabs.com/subgraphs/name/connext/nxtp-avalanche',
+  cache: new InMemoryCache(),
+  queryDeduplication: true,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-first',
+    },
+    query: {
+      fetchPolicy: 'cache-first',
+      errorPolicy: 'all',
+    },
+  },
+})
+// chains: [56, 137, 100, 250, 42161, 43114],
+
+export const apolloClients = (chainId: number) => {
+  switch (chainId) {
+    case 56:
+      return bscClient
+    case 100:
+      return xdaiClient
+    case 137:
+      return polygonClient
+    case 250:
+      return fantomClient
+    case 42161:
+      return arbitrumClient
+    case 43114:
+      return avalancheClient
+    default:
+      return undefined
+  }
+}
