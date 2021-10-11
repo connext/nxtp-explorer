a93540f3f191148d5d8fc78bb7912a7d187c6e3c
Sanchay Mittal <sanchaymittal@gmail.com>
2021-10-11 21:51:11 +0530

    add: reducer changes

--- src/state/protocol/reducer.ts
+++ src/state/protocol/reducer.ts
@@ -1,53 +1,40 @@
-import { currentTimestamp } from '../../utils/index'
 import { updateProtocolData, updateChartData, updateTransactions } from './actions'
 import { createReducer } from '@reduxjs/toolkit'
 import { ChartDayData, Transaction } from 'types'
-import { SupportedNetwork } from 'constants/networks'
 
-export interface ProtocolData {
-  // volume
-  volumeUSD: number
-  volumeUSDChange: number
+// export interface ProtocolData {
+//   // volume
+//   volumeUSD: number
+//   volumeUSDChange: number
 
-  // in range liquidity
-  tvlUSD: number
-  tvlUSDChange: number
+//   // in range liquidity
+//   tvlUSD: number
+//   tvlUSDChange: number
 
-  // fees
-  feesUSD: number
-  feeChange: number
+//   // fees
+//   feesUSD: number
+//   feeChange: number
 
-  // transactions
-  txCount: number
-  txCountChange: number
-}
+//   // transactions
+//   txCount: number
+//   txCountChange: number
+// }
 
 export interface ProtocolState {
-  [networkId: string]: {
+  protocolData: {
     // timestamp for last updated fetch
     readonly lastUpdated: number | undefined
     // overview data
-    readonly data: ProtocolData | undefined
+    // readonly data: ProtocolData | undefined
+    readonly totalVolume: number | undefined
     readonly chartData: ChartDayData[] | undefined
     readonly transactions: Transaction[] | undefined
   }
 }
 
 export const initialState: ProtocolState = {
-  [SupportedNetwork.ETHEREUM]: {
-    data: undefined,
-    chartData: undefined,
-    transactions: undefined,
-    lastUpdated: undefined,
-  },
-  [SupportedNetwork.ARBITRUM]: {
-    data: undefined,
-    chartData: undefined,
-    transactions: undefined,
-    lastUpdated: undefined,
-  },
-  [SupportedNetwork.OPTIMISM]: {
-    data: undefined,
+  protocolData: {
+    totalVolume: undefined,
     chartData: undefined,
     transactions: undefined,
     lastUpdated: undefined,
@@ -56,15 +43,13 @@ export const initialState: ProtocolState = {
 
 export default createReducer(initialState, (builder) =>
   builder
-    .addCase(updateProtocolData, (state, { payload: { protocolData, networkId } }) => {
-      state[networkId].data = protocolData
-      // mark when last updated
-      state[networkId].lastUpdated = currentTimestamp()
+    .addCase(updateProtocolData, (state, { payload: { totalVolume } }) => {
+      state.protocolData.totalVolume = totalVolume
     })
-    .addCase(updateChartData, (state, { payload: { chartData, networkId } }) => {
-      state[networkId].chartData = chartData
+    .addCase(updateChartData, (state, { payload: { chartData } }) => {
+      state.protocolData.chartData = chartData
     })
-    .addCase(updateTransactions, (state, { payload: { transactions, networkId } }) => {
-      state[networkId].transactions = transactions
+    .addCase(updateTransactions, (state, { payload: { transactions } }) => {
+      state.protocolData.transactions = transactions
     })
 )
